057007248943bd3d16f42793dcec59f46c2096b2
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2022-11-30 12:00:03 -0300

    Ajustando conteúdo.

---
+++ fundamentos/secao-04-introducao-a-JS/dia-01-introducao-a-js/script-3.js
@@ -0,0 +1,6 @@
+// Conteudo do video sobre tipos primitivos, tipagem dinamica e operacoes aritmeticas.
+let movie = "Avengers"; //string literal
+let score = 10; //number literal
+let isValid = true; // boolean 
+let name; // valor undefined
+let color = null; // valor nulo
