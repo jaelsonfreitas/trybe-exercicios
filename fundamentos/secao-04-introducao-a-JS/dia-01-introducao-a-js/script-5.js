17632c609b1493323194d2a959857ef7781a1bbd
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2022-11-30 18:03:08 -0300

    Ajustando pasta.

---
+++ fundamentos/secao-04-introducao-a-JS/dia-01-introducao-a-js/script-5.js
@@ -0,0 +1,11 @@
+const nota = 83;
+
+if (nota <= 59.99) {
+    console.log("Infelizmente, você REPROVOU.");
+}
+else if (nota >= 60 && nota<= 79.99) {
+    console.log("Você está em nossa lista de espera.");
+}
+else {
+    console.log("Parabéns, você faz parte do grupo das pessoas aprovadas!");
+}
