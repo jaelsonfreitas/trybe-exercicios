24175764c41eba9c8c0ab5634068b909c2d6b988
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2022-11-30 15:14:06 -0300

    Operadores.

--- fundamentos/secao-04-introducao-a-JS/dia-01-introducao-a-js/script-3.js
+++ fundamentos/secao-04-introducao-a-JS/dia-01-introducao-a-js/script-3.js
@@ -3,4 +3,12 @@ let movie = "Avengers"; //string literal
 let score = 10; //number literal
 let isValid = true; // boolean 
 let name; // valor undefined
-let color = null; // valor nulo
+let color = null; // valor redefinicão
+
+const base = 5;
+const heigth = 8
+const area = base * heigth
+const perimeter = 5 + 5 + 8 + 8
+console.log(perimeter)
+
+console.log(area)
