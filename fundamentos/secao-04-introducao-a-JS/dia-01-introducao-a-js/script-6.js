ace577dbff958f6a1e445a4a196c13f0fddfc401
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2022-12-01 22:07:37 -0300

    ajustando.

--- fundamentos/secao-04-introducao-a-JS/dia-01-introducao-a-js/script-6.js
+++ fundamentos/secao-04-introducao-a-JS/dia-01-introducao-a-js/script-6.js
@@ -25,3 +25,12 @@ console.log(true && true); // true
 console.log(true && false); // false
 console.log(false && true); // false
 console.log(false && false); // false
+
+const bebidaPrincipal =  'cafezinho';
+const bebidaAlternativa = 'suco de laranja';
+
+if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
+  console.log("Obrigado por me atender :D")
+} else {
+  console.log("Ei, eu não pedi isso!");
+}
