e9a62ebdb3987dfd6f9e4a664cb20d32e363fe8d
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2022-12-02 11:14:08 -0300

    fixar 1

--- fundamentos/secao-04-introducao-a-JS/dia-02-array-e-loop-for/arrays.js
+++ fundamentos/secao-04-introducao-a-JS/dia-02-array-e-loop-for/arrays.js
@@ -1,3 +1,20 @@
+
+
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);
+// Tomar café
+
+let searchForLastTask = tasksList[tasksList.length - 1];
+console.log(searchForLastTask);
+// Brincar com o cachorro
+
 let pizza = ['4 Queijos', 'Frango Catupiry', 'Marguerita', 'Palmito', 'Chocolate'];
 
 console.log(pizza);
+
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+console.log(tasksList.length);
+// 3
