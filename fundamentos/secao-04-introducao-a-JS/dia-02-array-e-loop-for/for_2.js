15ca4acfb4c434db2071607d4cf19c7a78d767ce
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2022-12-02 14:42:39 -0300

    Aula

---
+++ fundamentos/secao-04-introducao-a-JS/dia-02-array-e-loop-for/for_2.js
@@ -0,0 +1,5 @@
+var listaDeNomes = [ "Joana", "Maria", "Lucas"];
+for(var indice = 0; indice < listaDeNomes.length; indice++) {
+    var mensagem = "Boas Vindas, " + listaDeNomes[indice] + "!";
+    console.log(mensagem)
+}
