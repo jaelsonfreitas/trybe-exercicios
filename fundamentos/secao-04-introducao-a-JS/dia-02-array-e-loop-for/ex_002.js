140054d5109e6d1c000df4242089b56ebf14840a
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2022-12-04 10:55:09 -0300

    Ex 002.

---
+++ fundamentos/secao-04-introducao-a-JS/dia-02-array-e-loop-for/ex_002.js
@@ -0,0 +1,10 @@
+// some todos os valores do array e imprima o resultado.
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let = result = 0; // para somar os valores preciso criar uma variavel inicial.
+
+for (let soma = 0; soma < numbers.length ; soma += 1) {
+    result += numbers[soma];
+}
+
+console.log(result);
